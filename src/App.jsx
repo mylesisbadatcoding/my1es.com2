b9ccf14735b754da75bdb317d7841fface63c161
my1es <mylesrhinehartdavis@gmail.com>
2026-06-01 18:47:08 -0700

    hardoded key

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import 'tldraw/tldraw.css'
 export default function App() {
   return (
     <div style={{ position: 'fixed', inset: 0 }}>
-      <Tldraw licenseKey={import.meta.env.VITE_TLDRAW_LICENSE_KEY} />
+      <Tldraw licenseKey="tldraw-2027-05-14/WyJkalg1bUhMMyIsWyIqLm15MWVzLmNvbSJdLDksIjIwMjctMDUtMTQiXQ.Lot1IDKL0RSiAZAnU8mephe6PtZ63+7ZjCO+NIhpI3UUYsdMgv1/uNCCc5qDEtE7QNQ1imq88Wnk2HaSwR23Ww" />
     </div>
   )
 }
